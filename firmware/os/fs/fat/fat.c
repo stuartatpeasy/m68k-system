08757c83b39d87bf1454a89857e8650216854ee2
swallace <stuart@sign-up.to>
2015-07-29 09:43:39 +0100

    Initial structs for FAT fs driver

---
+++ firmware/os/fs/fat/fat.c
@@ -0,0 +1,10 @@
+/*
+    FAT16 file system support
+
+    Part of the as-yet-unnamed MC68010 operating system
+
+
+    (c) Stuart Wallace, July 2015
+*/
+
+#include "fat.h"
---
+++ firmware/os/fs/fat/fat.h
@@ -0,0 +1,16 @@
+#ifndef FS_FAT_FAT_INC
+#define FS_FAT_FAT_INC
+/*
+    FAT16 file system support
+
+    Part of the as-yet-unnamed MC68010 operating system
+
+
+    (c) Stuart Wallace, July 2015
+*/
+
+#include "include/defs.h"
+#include "include/types.h"
+
+
+#endif
