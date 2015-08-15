7f3324442b73f844e374336f3efdce0af3ac1495
stuartatpeasy <stuartw@atom.net>
2015-08-15 18:14:39 +0100

    adding source files for file-handling functions

---
+++ firmware/os/fs/file.c
@@ -0,0 +1,11 @@
+/*
+	File-handling functions
+
+	Part of the as-yet-unnamed MC68010 operating system
+
+
+	(c) Stuart Wallace <stuartw@atom.net>, August 2015.
+*/
+
+#include "fs/file.h"
+
---
+++ firmware/os/fs/file.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_FS_FILE_H_INC
+#define INCLUDE_FS_FILE_H_INC
+/*
+	File-handling functions
+
+	Part of the as-yet-unnamed MC68010 operating system
+
+
+	(c) Stuart Wallace <stuartw@atom.net>, August 2015.
+*/
+
+#endif
