5e9008a096a2dd4ea6c706873579ab914476e210
stuartatpeasy <stuartw@atom.net>
2015-11-11 14:11:57 +0000

    changes to build process: removed unnecessary dir from include search path; moved constant defs into buildcfg.h

---
+++ firmware/os/buildcfg.h
@@ -0,0 +1,22 @@
+#ifndef BUILDCFG_H_INC
+#define BUILDCFG_H_INC
+/*
+	buildcfg.h: configure a build for a particular platform and target, and set options.
+
+	Part of ayumos
+
+
+	(c) Stuart Wallace <stuartw@atom.net>, November 2015.
+
+*/
+
+
+#define KMALLOC_HEAP
+
+#define BUG_ATA_BYTE_SWAP
+
+#define TARGET_MC68010
+#define PLATFORM_LAMBDA_REV0
+
+#endif
+
