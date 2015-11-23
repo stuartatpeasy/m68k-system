dddedd7933f9a509631165e70967e5f7ade446ef
Stuart Wallace <stuartw@atom.net>
2015-11-23 09:52:45 +0000

    AVR Studio project and some initial firmware code for the ATmega8L microcontroller used in the PS/2 controller expansion card.

---
+++ hardware/Expansion cards/PS2 controller/PS2Controller/PS2Controller/registers.h
@@ -0,0 +1,17 @@
+#ifndef REGISTERS_H_
+#define REGISTERS_H_
+/*
+	registers.h - Register definitions for the PS/2 keyboard & mouse controller
+
+	This module contains firmware for an m68k-bus peripheral controller.
+
+
+	(c) Stuart Wallace <stuartw@atom.net>, November 2015.
+
+ */
+
+
+#define REG_KB_DATA			(0)
+#define REG_MOUSE_DATA		(1)
+
+#endif
