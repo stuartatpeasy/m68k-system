bf40616f25a38677517bec54d9ea2df3d374bb77
stuartatpeasy <stuartw@atom.net>
2016-02-21 20:44:38 +0000

    Added templates for network routing functions and updated project

---
+++ firmware/os/kernel/net/route.c
@@ -0,0 +1,10 @@
+/*
+    Network packet routing functions
+
+    Part of ayumos
+
+
+    (c) Stuart Wallace, February 2016.
+*/
+
+#include <kernel/net/route.h>
---
+++ firmware/os/kernel/net/route.h
@@ -0,0 +1,17 @@
+#ifndef KERNEL_NET_ROUTE_H_INC
+#define KERNEL_NET_ROUTE_H_INC
+/*
+    Network packet routing functions
+
+    Part of ayumos
+
+
+    (c) Stuart Wallace, February 2016.
+*/
+
+#include <kernel/include/defs.h>
+#include <kernel/include/types.h>
+
+
+
+#endif
