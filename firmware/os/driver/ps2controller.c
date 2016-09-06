c82bdeff52310bc9a2c63c9c9c9448eff1b8f7d2
stuartatpeasy <stuartw@atom.net>
2016-09-06 20:33:02 +0100

    Further work on the PS/2 controller driver

--- firmware/os/driver/ps2controller.c
+++ firmware/os/driver/ps2controller.c
@@ -14,49 +14,15 @@
 #include <klibc/stdio.h>			/* TODO remove */
 
 
+void ps2controller_port_irq_handler(ku32 irql, void *data);
 s32 ps2controller_port_a_init(dev_t *dev);
 s32 ps2controller_port_b_init(dev_t *dev);
-void ps2controller_irq_handler(ku32 irql, void *data);
+s32 ps2controller_port_init(dev_t *dev, ku16 reg_offset);
+s32 ps2controller_port_shut_down(dev_t *dev);
 s32 ps2controller_control(dev_t *dev, const devctl_fn_t fn, const void *in, void *out);
 s32 ps2controller_read(dev_t *dev, ku32 offset, u32 *len, void *buf);
 s32 ps2controller_write(dev_t *dev, ku32 offset, u32 *len, const void *buf);
 s32 ps2controller_shut_down(dev_t *dev);
-
-
-/*
-    ps2controller_irq_handler() - interrupt service routine
-*/
-void ps2controller_irq_handler(ku32 irql, void *data)
-{
-    dev_t * const dev = (dev_t *) data;
-    void * const base_addr = dev->base_addr;
-    u8 status_a, status_b;
-    UNUSED(irql);
-    UNUSED(data);
-
-    status_a = PS2CTRLR_REG(base_addr, PS2_STATUS_A);
-    status_b = PS2CTRLR_REG(base_addr, PS2_STATUS_B);
-
-    if(status_a)
-    {
-        if(status_a & PS2_FLAG_RX)
-        {
-            /* Handle received data */
-        }
-
-        /* Clear all channel A interrupt flags */
-        PS2CTRLR_REG(base_addr, PS2_STATUS_A) = 0;
-    }
-
-    if(status_b)
-    {
-
-        /* Clear all channel B interrupt flags */
-        PS2CTRLR_REG(base_addr, PS2_STATUS_B) = 0;
-    }
-
-    putchar('#');
-}
 
 
 /*
@@ -65,8 +31,12 @@ void ps2controller_irq_handler(ku32 irql, void *data)
 s32 ps2controller_init(dev_t *dev)
 {
     void * const base_addr = dev->base_addr;
-    dev_t *port_a, *port_b;
+    ps2controller_state_t *state;
     s32 ret;
+
+    state = CHECKED_KCALLOC(1, sizeof(ps2controller_state_t));
+
+    dev->data = state;
 
     dev->shut_down  = ps2controller_shut_down;
     dev->read       = ps2controller_read;
@@ -76,30 +46,29 @@ s32 ps2controller_init(dev_t *dev)
     dev->data = NULL;
 
     /* Set up child devices */
-    ret = dev_create(DEV_TYPE_CHARACTER, DEV_SUBTYPE_PS2PORT, dev->name, IRQL_NONE, dev->base_addr,
-                        &port_a, "PS/2 port A", dev, ps2controller_port_a_init);
-    if(ret != SUCCESS)
-        return ret;
-
-    ret = dev_create(DEV_TYPE_CHARACTER, DEV_SUBTYPE_PS2PORT, dev->name, IRQL_NONE, dev->base_addr,
-                        &port_b, "PS/2 port B", dev, ps2controller_port_b_init);
+    ret = dev_create(DEV_TYPE_CHARACTER, DEV_SUBTYPE_PS2PORT, dev->name, dev->irql, dev->base_addr,
+                        &state->port_a, "PS/2 port A", dev, ps2controller_port_a_init);
     if(ret != SUCCESS)
     {
-        dev_destroy(port_a);
+        kfree(state);
+        dev->data = NULL;
+        return ret;
+    }
+
+    ret = dev_create(DEV_TYPE_CHARACTER, DEV_SUBTYPE_PS2PORT, dev->name, dev->irql, dev->base_addr,
+                        &state->port_b, "PS/2 port B", dev, ps2controller_port_b_init);
+    if(ret != SUCCESS)
+    {
+        dev_destroy(state->port_a);
+        kfree(state);
+        dev->data = NULL;
         return ret;
     }
 
     /* Switch port power on */
     PS2CTRLR_REG(base_addr, PS2_CFG) = PS2_CFG_PWR_A | PS2_CFG_PWR_B;
 
-    /* Note: interrupts are disabled during initialisation.  We therefore poll for peripherals. */
-
-    cpu_irq_add_handler(dev->irql, dev, ps2controller_irq_handler);
-
-    /* Enable interrupts */
-    PS2CTRLR_REG(base_addr, PS2_INT_CFG_A) = PS2_FLAG_RX | PS2_FLAG_PAR_ERR | PS2_FLAG_OVF;
-    PS2CTRLR_REG(base_addr, PS2_INT_CFG_B) = PS2_FLAG_RX | PS2_FLAG_PAR_ERR | PS2_FLAG_OVF;
-
+    /* Activate the controller's global interrupt enable */
     PS2CTRLR_REG(base_addr, PS2_CFG) |= PS2_CFG_IE;
 
     return SUCCESS;
@@ -111,8 +80,7 @@ s32 ps2controller_init(dev_t *dev)
 */
 s32 ps2controller_port_a_init(dev_t *dev)
 {
-    UNUSED(dev);
-    return SUCCESS;
+    return ps2controller_port_init(dev, PS2_PORT_A_REG_OFFSET);
 }
 
 
@@ -121,23 +89,175 @@ s32 ps2controller_port_a_init(dev_t *dev)
 */
 s32 ps2controller_port_b_init(dev_t *dev)
 {
-    UNUSED(dev);
+    return ps2controller_port_init(dev, PS2_PORT_B_REG_OFFSET);
+}
+
+
+/*
+    ps2controller_port_init() - initialise the specified port of the PS/2 controller.
+*/
+s32 ps2controller_port_init(dev_t *dev, ku16 reg_offset)
+{
+    ps2controller_port_state_t *state = CHECKED_KCALLOC(1, sizeof(ps2controller_port_state_t));
+
+    state->err = 0;
+
+    state->regs.data    = PS2CTRLR_REG_ADDR(dev->base_addr, reg_offset + PS2_DATA);
+    state->regs.status  = PS2CTRLR_REG_ADDR(dev->base_addr, reg_offset + PS2_STATUS);
+    state->regs.int_cfg = PS2CTRLR_REG_ADDR(dev->base_addr, reg_offset + PS2_INT_CFG);
+
+    CIRCBUF_INIT(state->tx_buf);
+
+    dev->data = state;
+
+    cpu_irq_add_handler(dev->irql, dev, ps2controller_port_irq_handler);
+
+    /* Enable interrupts */
+    *state->regs.int_cfg = PS2_FLAG_RX | PS2_FLAG_PAR_ERR | PS2_FLAG_OVF;
+
     return SUCCESS;
 }
 
 
 /*
-    ps2controller_shut_down() - prepare for deletion of the device.
+    ps2controller_port_irq_handler() - handle the interrupt raised by the flags in the register
+    specified by reg.
+*/
+void ps2controller_port_irq_handler(ku32 irql, void *data)
+{
+    dev_t * const dev = (dev_t *) data;
+    ps2controller_port_state_t *state = (ps2controller_port_state_t *) dev->data;
+    UNUSED(irql);
+
+    ku8 status = *state->regs.status;
+    if(status)
+    {
+        if(status & PS2_FLAG_RX)        // change to while()
+        {
+            ku8 key = *state->regs.data;
+
+            if(key == PS2_SC_KB_EXT1)
+                state->packet.kb.flags |= PS2_PKT_KB_EXT1;
+            else if(key == PS2_SC_KB_EXT2)
+                state->packet.kb.flags |= PS2_PKT_KB_EXT2;
+            else if(key == PS2_SC_KB_RELEASE)
+                state->packet.kb.flags |= PS2_PKT_KB_RELEASE;
+            else
+            {
+                ku8 flags = state->packet.kb.flags & (PS2_PKT_KB_EXT1 | PS2_PKT_KB_EXT2);
+
+                state->packet.kb.data = (state->packet.kb.data << 8) | key;
+
+                switch(flags)
+                {
+                    case 0:
+                        /* If neither of the EXTx flags is true, this must be a single-byte code */
+                        ps2controller_process_key(state);
+                        break;
+
+                    case PS2_PKT_KB_EXT1:
+                        /*
+                            Most EXT1-prefixed keys are single-byte codes; two aren't.  We see
+                            "PrtSc pressed" as 0x127c, and "PrtSc released" as 0x7c12", with the
+                            appropriate flags set.  We defer processing the key if we have received
+                            only the first byte of either of these two (0x127c / 0x7c12) scan codes.
+                            Otherwise, the scan code is processed now.
+                        */
+                        if(((flags & PS2_SC_KB_RELEASE) && (state->packet.kb.data != 0x7c)) ||
+                           (!(flags & PS2_SC_KB_RELEASE) && (state->packet.kb.data != 0x12)))
+                            ps2controller_process_key(state);
+                        break;
+
+                    case PS2_PKT_KB_EXT2:
+                        /*
+                            The only EXT2-prefixed code we recognise is "Pause pressed", which we
+                            receive as 0x14771477.  The scan code is actually 0xe1, 0x14, 0x77,
+                            0xe1, 0xf0, 0x14, 0xf0, 0x77.  Note that misplaced prefixes will not be
+                            detected.  If we have received any four bytes (not including the prefix
+                            characters 0xe1 and 0xf0), we process the key.
+                        */
+                        if((flags & PS2_SC_KB_RELEASE) && (state->packet.kb.data & 0xff00000))
+                            ps2controller_process_key(state);
+                        break;
+
+                    case PS2_PKT_KB_EXT1 | PS2_PKT_KB_EXT2:
+                        /* Invalid state - discard packet */
+                        break;
+                }
+
+                state->packet.kb.data = 0;
+                state->packet.kb.flags = 0;
+            }
+        }
+        else if(status & PS2_FLAG_TX)
+        {
+            /* TODO: send next byte in TX buffer, if any */
+            putchar('t');
+        }
+        else if(status & PS2_ERR_MASK)
+        {
+            state->err |= status & PS2_ERR_MASK;
+            printf("[ps2e: %02x]", status & PS2_ERR_MASK);      // FIXME remove this
+        }
+
+        *state->regs.status = 0;
+    }
+}
+
+
+void ps2controller_process_key(ps2controller_port_state_t *state)
+{
+    if(state->packet.kb.flags & PS2_PKT_KB_RELEASE)
+        putchar('!');
+
+    if(state->packet.kb.flags & PS2_PKT_KB_EXT1)
+        putchar('1');
+
+    if(state->packet.kb.flags & PS2_PKT_KB_EXT2)
+        putchar('2');
+
+    printf(":%08x ", state->packet.kb.data);
+}
+
+
+/*
+    ps2controller_shut_down() - prepare for deletion of the controller device.  Note that a separate
+    function exists for shutting down the sub-devices representing each of the PS/2 ports.
+    This function powers down the PS/2 ports and clears the controller's global interrupt enable
+    flag.
 */
 s32 ps2controller_shut_down(dev_t *dev)
 {
-    void * const base_addr = dev->base_addr;
-
     /* Disable interrupts and deactivate power to both ports */
-    PS2CTRLR_REG(base_addr, PS2_CFG) &= ~(PS2_CFG_PWR_A | PS2_CFG_PWR_B | PS2_CFG_IE);
+    PS2CTRLR_REG(dev->base_addr, PS2_CFG) &= ~(PS2_CFG_PWR_A | PS2_CFG_PWR_B | PS2_CFG_IE);
 
     if(dev->data != NULL)
+    {
         kfree(dev->data);
+        dev->data = NULL;
+    }
+
+    return SUCCESS;
+}
+
+
+/*
+    ps2controller_port_shut_down() - prepare for deletion of the port device.  This function
+    disables interrupts from the port.
+*/
+s32 ps2controller_port_shut_down(dev_t *dev)
+{
+    ps2controller_port_state_t * const state = (ps2controller_port_state_t *) dev->data;
+    if(state != NULL)
+    {
+        /* Disable all interrupts from the port */
+        *state->regs.int_cfg = 0;
+
+        cpu_irq_remove_handler(dev->irql, ps2controller_port_irq_handler, dev->data);
+
+        kfree(dev->data);
+        dev->data = NULL;
+    }
 
     return SUCCESS;
 }
--- firmware/os/driver/ps2controller.h
+++ firmware/os/driver/ps2controller.h
@@ -15,6 +15,7 @@
 #include <kernel/include/defs.h>
 #include <kernel/include/error.h>
 #include <kernel/include/types.h>
+#include <kernel/util/buffer.h>
 
 
 /*
@@ -34,10 +35,102 @@
 
 /* Given a peripheral based at address "base", obtain the address of register "r" */
 #define PS2CTRLR_REG_ADDR(base, r)   \
-    (((u32) (base)) + PS2CTRLR_BASE + ((r) << PS2CTRLR_SHIFT) + PS2CTRLR_OFFSET)
+    ((vu8 *) (((u32) (base)) + PS2CTRLR_BASE + ((r) << PS2CTRLR_SHIFT) + PS2CTRLR_OFFSET))
 
 /* Accessor for register "r" in a peripheral based at address "base" */
-#define PS2CTRLR_REG(base, r)    *((vu8 *) PS2CTRLR_REG_ADDR((base), (r)))
+#define PS2CTRLR_REG(base, r)    (*PS2CTRLR_REG_ADDR((base), (r)))
+
+
+/* Flags associated with packets of data received from a port */
+#define PS2_PKT_KB_RELEASE      BIT(7)  /* Key-release prefix received                          */
+#define PS2_PKT_KB_EXT1         BIT(6)  /* First extended-key prefix flag (0xe0) received       */
+#define PS2_PKT_KB_EXT2         BIT(5)  /* Second extended-key prefix flag (0xe1) received      */
+
+/* "Special" keyboard scan code component bytes */
+#define PS2_SC_KB_RELEASE       (0xf0)  /* Key-release event                                    */
+#define PS2_SC_KB_EXT1          (0xe0)  /* First extended key set                               */
+#define PS2_SC_KB_EXT2          (0xe1)  /* Second extended key set                              */
+
+
+/* State of a single PS/2 port */
+typedef struct
+{
+    struct
+    {
+        vu8 *   data;
+        vu8 *   status;
+        vu8 *   int_cfg;
+    } regs;
+
+    struct
+    {
+        struct
+        {
+            u32     data;
+            u8      flags;
+        } kb;
+    } packet;
+
+    u8                      err;
+    CIRCBUF(u8)             tx_buf;
+} ps2controller_port_state_t;
+
+
+/* Overall controller state */
+typedef struct
+{
+    dev_t   *port_a;
+    dev_t   *port_b;
+} ps2controller_state_t;
+
+
+/* PS/2 port state machine states */
+enum PS2Port_State
+{
+    PS2_STATE_INIT,
+    PS2_STATE_RESET_SENT,
+    PS2_STATE_RESET_ACK_RECEIVED,
+
+};
+
+
+/*
+    PS/2 commands.  Note that many of these apply only to a keyboard, or only to a mouse
+*/
+/* PS/2 commands which apply to keyboard and mouse */
+#define PS2_CMD_RESET                   (0xff)
+#define PS2_CMD_RESEND                  (0xfe)
+#define PS2_CMD_SET_DEFAULTS            (0xf6)
+#define PS2_CMD_DISABLE                 (0xf5)
+#define PS2_CMD_ENABLE                  (0xf4)
+#define PS2_CMD_READ_ID                 (0xf2)
+
+/* PS/2 keyboard-specific commands */
+#define PS2_CMD_SET_TYPEMATIC_RATE      (0xf3)
+#define PS2_CMD_SET_KEY_TYPE_MAKE       (0xfd)
+#define PS2_CMD_SET_KEY_TYPE_MAKE_BREAK (0xfc)
+#define PS2_CMD_SET_KEY_TYPE_TYPEMATIC  (0xfb)
+#define PS2_CMD_SET_ALL_KEYS_NORMAL     (0xfa)
+#define PS2_CMD_SET_ALL_KEYS_MAKE       (0xf9)
+#define PS2_CMD_SET_ALL_KEYS_MAKE_BREAK (0xf8)
+#define PS2_CMD_SET_ALL_KEYS_TYPEMATIC  (0xf7)
+#define PS2_CMD_SET_SCAN_CODE_SET       (0xf0)
+#define PS2_CMD_ECHO                    (0xee)
+#define PS2_CMD_SET_LEDS                (0xed)
+
+/* PS/2 mouse-specific commands */
+#define PS2_CMD_SET_SAMPLE_RATE         (0xf3)
+#define PS2_CMD_SET_REMOTE_MODE         (0xf0)
+#define PS2_CMD_SET_WRAP_MODE           (0xee)
+#define PS2_CMD_RESET_WRAP_MODE         (0xec)
+#define PS2_CMD_READ_DATA               (0xeb)
+#define PS2_CMD_SET_STREAM_MODE         (0xea)
+#define PS2_CMD_STATUS_REQUEST          (0xe9)
+
+
+/* Scan code sent to prefix a "key release" code */
+#define PS2_SC_RELEASE                  (0xf0)
+
 
 /* PS/2 controller registers */
 enum PS2Controller_Reg
@@ -51,6 +144,21 @@ enum PS2Controller_Reg
     PS2_INT_CFG_B   = 0x06,
     PS2_UNUSED      = 0x07
 };
+
+
+/* Port-specific register base address offset*/
+#define PS2_PORT_A_REG_OFFSET   (0)
+#define PS2_PORT_B_REG_OFFSET   (4)
+
+
+/* Offsets of port-specific registers from port base address */
+enum PS2Controller_PortRegOffset
+{
+    PS2_DATA    = 0x00,
+    PS2_STATUS  = 0x01,
+    PS2_INT_CFG = 0x02
+};
+
 
 /*
     Individual register bits
@@ -66,10 +174,14 @@ enum PS2Controller_Reg
 #define PS2_FLAG_START_TIMEOUT  BIT(1)     /* Timeout while waiting for TX clock                */
 #define PS2_FLAG_CMD_TIMEOUT    BIT(0)     /* Timeout waiting for command response              */
 
+/* Mask representing all possible error constants */
+#define PS2_ERR_MASK    (PS2_FLAG_PAR_ERR | PS2_FLAG_OVF | PS2_FLAG_RX_TIMEOUT | \
+                         PS2_FLAG_TX_TIMEOUT | PS2_FLAG_START_TIMEOUT | PS2_FLAG_CMD_TIMEOUT)
+
 /* Bits in PS2_CFG */
 #define PS2_CFG_IE              BIT(7)     /* Global interrupt enable                           */
-#define PS2_CFG_PWR_A           BIT(4)     /* Enable/disable power to channel A                 */
-#define PS2_CFG_PWR_B           BIT(3)     /* Enable/disable power to channel B                  */
+#define PS2_CFG_PWR_A           BIT(4)     /* Enable/disable power to port A                    */
+#define PS2_CFG_PWR_B           BIT(3)     /* Enable/disable power to port B                    */
 
 
 s32 ps2controller_init(dev_t *dev);
