f439559bd88da9c765dfd54089711a9b035d9506
Stuart Wallace <stuartw@atom.net>
2017-04-26 15:39:10 +0000

    Added support for compiling C++ kernel sources.  Note that the viability of C++ code has not yet been tested.

--- ayumos/global.mk
+++ ayumos/global.mk
@@ -2,18 +2,21 @@
 ARCH=m68000
 PLATFORM=lambda
 
-# TOOLDIR is the base directory in which the cross-compiler toolchain is installed
-TOOLDIR=/opt/m68k
-TOOLPREFIX=m68k-elf
+# TOOL_ROOT is the base directory in which the cross-compiler toolchain is installed
+TOOL_ROOT=/opt/m68k
+TOOL_BINDIR=$(TOOL_ROOT)/bin
+TOOL_ARCH=m68k-elf
+TOOL_PREFIX=$(TOOL_ARCH)-
 
 # Set up paths to the cross-compiler binaries
-TARGET_CC=$(TOOLDIR)/bin/$(TOOLPREFIX)-gcc
-TARGET_AS=$(TOOLDIR)/bin/$(TOOLPREFIX)-as
-TARGET_LD=$(TOOLDIR)/bin/$(TOOLPREFIX)-ld
-TARGET_CPP=$(TOOLDIR)/bin/$(TOOLPREFIX)-cpp
-TARGET_OBJCOPY=$(TOOLDIR)/bin/$(TOOLPREFIX)-objcopy
-TARGET_AR=$(TOOLDIR)/bin/$(TOOLPREFIX)-ar
-TARGET_NM=$(TOOLDIR)/bin/$(TOOLPREFIX)-nm
+TARGET_CC=$(TOOL_BINDIR)/$(TOOL_PREFIX)gcc
+TARGET_CXX=$(TOOL_BINDIR)/$(TOOL_PREFIX)g++
+TARGET_AS=$(TOOL_BINDIR)/$(TOOL_PREFIX)as
+TARGET_LD=$(TOOL_BINDIR)/$(TOOL_PREFIX)ld
+TARGET_CPP=$(TOOL_BINDIR)/$(TOOL_PREFIX)cpp
+TARGET_OBJCOPY=$(TOOL_BINDIR)/$(TOOL_PREFIX)objcopy
+TARGET_AR=$(TOOL_BINDIR)/$(TOOL_PREFIX)ar
+TARGET_NM=$(TOOL_BINDIR)/$(TOOL_PREFIX)nm
 
 TARGET_GCC_VERSION=$(shell $(TARGET_CC) --version 2>&1 | head -n 1 | cut -d' ' -f3)
 
@@ -25,11 +28,15 @@ LD=/usr/bin/ld
 APPNAME=ayumos
 
 # Path to libgcc.a for the target architecture
-LIBGCCDIR=$(TOOLDIR)/lib/gcc/$(TOOLPREFIX)/$(TARGET_GCC_VERSION)/$(ARCH)
+LIBGCCDIR=$(TOOL_ROOT)/lib/gcc/$(TOOL_ARCH)/$(TARGET_GCC_VERSION)/$(ARCH)
 
 # Target compile/link options
 TARGET_CFLAGS=-I. -Iklibc -Wall -Wextra -O2 -$(ARCH) -g -include buildcfg.h -ffreestanding \
               -fomit-frame-pointer -fno-delete-null-pointer-checks
+
+# [2017-04-26] Exceptions are disabled in C++ code until I work out how to deal with the generated
+# .eh_frame section.
+TARGET_CXXFLAGS=$(TARGET_CFLAGS) -fno-exceptions
 
 TARGET_LDFLAGS=-g -T $(LDSCRIPT) -nostdlib -static -L$(LIBGCCDIR) -L.
 
