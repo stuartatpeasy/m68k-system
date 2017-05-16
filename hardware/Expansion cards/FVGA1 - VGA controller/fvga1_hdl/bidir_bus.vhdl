25944c28d78e4104327ccb719b6f6a89eb44f7f1
Stuart Wallace <stuartw@atom.net>
2017-05-16 18:27:59 +0000

    Modified/fixed the declaration of the bidir_bus VHDL module

--- hardware/Expansion cards/FVGA1 - VGA controller/fvga1_hdl/bidir_bus.vhdl
+++ hardware/Expansion cards/FVGA1 - VGA controller/fvga1_hdl/bidir_bus.vhdl
@@ -16,24 +16,23 @@ entity bidir_bus is
     port(
         PIN     : inout std_logic_vector(width - 1 downto 0);   -- port "pins" (bidirectional)
 
-        READ    : out    std_logic_vector(width - 1 downto 0);  -- read port (input from pins)
-        WRITE   : in     std_logic_vector(width - 1 downto 0);  -- write port (output to pins)
+        READ    : out   std_logic_vector(width - 1 downto 0);   -- read port (input from pins)
+        WRITE   : in    std_logic_vector(width - 1 downto 0);   -- write port (output to pins)
 
-        ENABLE  : in std_logic                                  -- output enable
+        ENABLE  : in    std_logic                               -- output enable
     );
 end bidir_bus;
 
 
 architecture behaviour of bidir_bus is
 begin
-    process(ENABLE, PIN, WRITE)
+    process(ENABLE, WRITE)
     begin
         if(ENABLE = '1') then
-            PIN   <= WRITE;
-            READ  <= (others => 'Z');
+            PIN <= WRITE;
         else
-            READ  <= PIN;
-            PIN   <= (others => 'Z');
+            PIN <= (others => 'Z');
         end if;
+        READ <= PIN;
     end process;
 end behaviour;
